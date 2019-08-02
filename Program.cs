ae3cb2f4927a866abeb4e48df7cbc4ca4b9197b5
Pavel Linkevich <pavellinkrvich2003@gmail.com>
2019-08-02 14:38:51 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTask
+{
+    class Program
+    {
+        static int[] array = new int[20];
+        static void Main(string[] args)
+        {
+            EvenFibonachiNumbers();
+        }
+        static void EvenFibonachiNumbers()
+        {
+            array[0] = 0;
+            array[1] = 1;
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            for (int i = 2; i < 20; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
